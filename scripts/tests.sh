a567d971dd5b43306dfa9efc4b0cad2ea0153f32
Tomaz Muraus <tomaz@tomaz.me>
2012-07-26 14:34:07 -0700

    Use different dependencies file with higher timeout when running on travis-ci.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,8 +2,14 @@ if [ ! $TEST_FILES ]; then
   TEST_FILES=$(find tests/ -type f -name "test-*.js" -print0 | tr "\0" " " | sed '$s/.$//')
 fi
 
+if [ $TRAVIS ]; then
+  DEPENDENCIES_FILE="tests/dependencies-travis.json"
+else
+  DEPENDENCIES_FILE="tests/dependencies.json"
+fi
+
 NODE_PATH=lib node_modules/whiskey/bin/whiskey \
   --tests "${TEST_FILES}" \
   --timeout 50000 \
-  --dependencies "tests/dependencies.json" \
+  --dependencies $DEPENDENCIES_FILE \
   --real-time --sequential
